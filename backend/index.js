4b14cf51381721035d94e8bb01ac45ddf1cb82cd
sayamjn <sayamjain0708@gmail.com>
2023-03-10 19:20:09 +0530

    added npm packages

--- backend/index.js
+++ backend/index.js
@@ -43,7 +43,7 @@ app.post("/generate", async (req, res) => {
    }
 });
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
    console.log(`Server is listening on port ${port}`);
