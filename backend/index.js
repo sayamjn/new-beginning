86b05b54333ef60aa6dab723c23bf62cc7fef31b
sayamjn <sayamjain0708@gmail.com>
2023-03-10 19:06:09 +0530

    react commit

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,50 @@
-// const express = require("express");
-import  express  from "express";
+import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import { Configuration, OpenAIApi } from "openai";
+
 dotenv.config();
+const app = express();
 
-const app = express()
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+const configuration = new Configuration({
+   apiKey: process.env.OPENAI_API_KEY,
+});
 
+const openai = new OpenAIApi(configuration);
 
-app.get("/",(req,res,next)=>{
-    return res.status(200).send("server is up")
-})
+app.get("/", (req, res) => {
+   return res.status(200).send("Server is up");
+});
 
+app.post("/generate", async (req, res) => {
+   const { prompt, size } = req.body;
+   //    validation
+   if (!prompt || !size) {
+      return res.status(400).send("Bad Request");
+   }
+   try {
+      const response = await openai.createImage({
+         prompt,
+         size,
+         n: 1,
+      });
+      const image_url = response.data.data[0].url;
 
+      return res.status(200).send({
+         src: image_url,
+      });
+   } catch (error) {
+      console.log(error);
+      return res.status(500).send({ error });
+   }
+});
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 
-app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`)
-})
+app.listen(port, () => {
+   console.log(`Server is listening on port ${port}`);
+});
