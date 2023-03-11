afa97c93403bdcc16f3e1fe5e88433f01b08f914
sayamjn <sayamjain0708@gmail.com>
2023-03-11 16:27:57 +0530

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import Navbar from './components/Navbar/Navbar'
+
 
 
 const App = () => {
   return (
     <div>
-    app
+    <Navbar/>
       
     </div>
   )
